bcf2e66e66d659eac02d865730135379cc391a93
mindthediv <gabriele98.contact@gmail.com>
2023-02-06 15:16:00 +0100

    new file:   Week-2/esercizioG1/assets/css/style.css 	new file:   Week-2/esercizioG1/assets/img/js.png 	new file:   Week-2/esercizioG1/assets/js/linked_a.js 	new file:   Week-2/esercizioG1/assets/js/linked_b.js 	new file:   Week-2/esercizioG1/index.html 	new file:   Week-2/esercizioG1/linked.html

---
+++ Week-2/esercizioG1/assets/js/linked_a.js
@@ -0,0 +1,5 @@
+window.alert('Primo Pop-up');
+alert('Secondo ' + 'Pop-up');
+
+document.write('<div><h1>Iniziamo la nostra pratica</h1><span>output scritto da Javascript</span><span id="spanVar"></span></div>');
+
---
+++ Week-2/esercizioG1/assets/js/linked_b.js
@@ -0,0 +1,10 @@
+var firstPet = 'pantera'
+var second_Pet = 'criceto'
+var pet3 = 'giraffa'
+var Pet = 'leone'
+
+document.getElementById('spanVar').innerHTML = firstPet + ' scritto da Javascript'
+
+console.log(second_Pet);
+console.log(pet3);
+console.log(Pet);
