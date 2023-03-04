1c2d01881faa43e256122044ed17071ac3197fa5
mindthediv <gabriele98.contact@gmail.com>
2023-03-04 02:04:05 +0100

    fixed: avatar pic btn color change on scroll cloning core el heights

--- Week-5/esercizioS5/assets/js/index.js
+++ Week-5/esercizioS5/assets/js/index.js
@@ -4,8 +4,10 @@ document.addEventListener('scroll', () => {
     let scrollPosY = window.scrollY;
         if (scrollPosY > 320) {
         rootEl.style.setProperty('--bg_color','white');
+        rootEl.style.setProperty('--btn_color','#1a8917');
     }
     else {
         rootEl.style.setProperty('--bg_color','#ffc017');
+        rootEl.style.setProperty('--btn_color','#191919');
     }
 });
