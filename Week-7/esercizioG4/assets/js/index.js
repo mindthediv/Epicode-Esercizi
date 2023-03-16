58537b36a842d7d013a78af1154eb4bff350d46a
mindthediv <gabriele98.contact@gmail.com>
2023-03-16 16:49:13 +0100

    esercizioG4

---
+++ Week-7/esercizioG4/assets/js/index.js
@@ -0,0 +1,48 @@
+/////////////////////////////////
+// TOTALMENTE DA RIAPPROCCIARE //
+/////////////////////////////////
+const myApiKey = "xUCVsn0uVXXtttTKtNLU83kSyPtmXkvllfGGIogt7YBpVFRzuijcf2mN";
+// { inserisci {authorization: '$myApiKey' } negli header della query
+const btnLoadImg = document.getElementById("btnLoad");
+const btnLoad2 = document.getElementById("btnload2");
+const svgArray = document.querySelectorAll(".card-img-top");
+const cardArray = document.querySelectorAll(".col-md-4 .card");
+let i = 0;
+
+const getImgs = async function (category) {
+  const albumURL = `https://api.pexels.com/v1/search?query=${category}`;
+  try {
+    const response = await fetch(albumURL, {
+      Method: "GET",
+      headers: {
+        Authorization:
+          "xUCVsn0uVXXtttTKtNLU83kSyPtmXkvllfGGIogt7YBpVFRzuijcf2mN",
+      },
+    });
+    const apiPic = await response.json();
+    svgArray.forEach((el) => el.remove());
+
+    console.log(apiPic);
+
+    cardArray.forEach((el) => {
+      let newImg = document.createElement("img");
+
+      let photoArray = apiPic.photos[i];
+      console.log(photoArray);
+      newImg.setAttribute("src", photoArray.src.medium);
+      i += 1;
+
+      let cardChild1 = el.querySelector(".card-body");
+      el.insertBefore(newImg, cardChild1);
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+btnLoadImg.addEventListener("click", () => {
+  getImgs("abstract");
+});
+btnLoad2.addEventListener("click", () => {
+  getImgs("music");
+});
